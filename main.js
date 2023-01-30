c8110154251cb10c8947e6f2b1c5d2cd3908615a
SHOR00 <jorgedrios86@gmail.com>
2023-01-30 17:25:37 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+ScrollReveal().reveal('.cabecera');
+ScrollReveal().reveal('.my', {delay:300});
+ScrollReveal().reveal('.tecnology', {delay:400});
+ScrollReveal().reveal('.proyecto',{delay:500});
+
+
+
+
